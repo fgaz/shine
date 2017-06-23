e4a464a0e855170470707752d3a421d10120014d
Francesco Gazzetta <francygazz@gmail.com>
2017-06-23 11:45:33 +0200

    adapted simpleInteraction.hs test to the new types

--- tests/simpleInteraction.hs
+++ tests/simpleInteraction.hs
@@ -1,13 +1,25 @@
+{-# LANGUAGE CPP #-}
+
 import Graphics.Shine
 import Graphics.Shine.Input
 import Graphics.Shine.Picture
 
-import GHCJS.DOM (webViewGetDomDocument, runWebGUI)
+import GHCJS.DOM (currentWindowUnchecked, currentDocument)
+
+#if defined(ghcjs_HOST_OS)
+run :: a -> a
+run = id
+#elif defined(MIN_VERSION_jsaddle_wkwebview)
+import Language.Javascript.JSaddle.WKWebView (run)
+#else
+import Language.Javascript.JSaddle.WebKitGTK (run)
+#endif
 
 main :: IO ()
-main = runWebGUI $ \ webView -> do
+main = run $ do
+    webView <- currentWindowUnchecked
     ctx <- fixedSizeCanvas webView 800 600
-    Just doc <- webViewGetDomDocument webView
+    Just doc <- currentDocument
     play ctx doc 30 initialState draw handleInput step
   where
     initialState = False
